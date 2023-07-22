4f3074e87158aac834b550c580c90eb1814958fe
vgulerianb <90599235+vgulerianb@users.noreply.github.com>
2023-07-22 14:39:27 +0530

    Action screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,22 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        fadeOut: {
+          "0%": { opacity: 1 },
+          "100%": {
+            opacity: 0,
+          },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.5s ease-in-out",
+        fadeOut: "fadeOut 0.5s ease-in-out",
+      },
     },
   },
   plugins: [],
